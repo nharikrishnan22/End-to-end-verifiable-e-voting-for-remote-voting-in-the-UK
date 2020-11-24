d276b901233eddbe077817fa60f59a6b690b9858
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2020-11-24 04:56:39 +0000

    Will try implementing this another way

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,2 @@
 class ApplicationController < ActionController::Base
-    helper_method :current_user
-    def current_user
-      if session[:user_id]
-        @_current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
-      else
-        @current_user = nil
-      end
-    end
-  end
+end
