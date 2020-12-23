c4987279c7390146ed6d4b20810b8f0f2a56f58b
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2020-12-23 18:01:27 +0000

    Can now upload avatar for new users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.require(:user).permit(:email, :dateofbirth, :firstname, :lastname, :password, :password_confirmation)
+      params.require(:user).permit(:email, :dateofbirth, :firstname, :lastname, :password, :password_confirmation, :avatar)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   
   has_secure_password
+  has_one_attached :avatar
   validates :email, presence: true, uniqueness: true # make sure that same email cannot be used twice
   validates :firstname, presence: true
   validates :lastname, presence: true
