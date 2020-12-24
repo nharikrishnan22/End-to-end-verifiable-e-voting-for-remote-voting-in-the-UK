3adbcd33c09f505de8ac2433723f11c64bdc2cf1
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2020-12-24 01:35:50 +0000

    Including user signatures is compulsory

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,9 @@
+# Based on https://medium.com/@wintermeyer/authentication-from-scratch-with-rails-5-2-92d8676f6836
 class User < ApplicationRecord
   
   has_secure_password
   has_one_attached :avatar
+  validates :avatar, presence: true
   validates :email, presence: true, uniqueness: true # make sure that same email cannot be used twice
   validates :firstname, presence: true
   validates :lastname, presence: true
