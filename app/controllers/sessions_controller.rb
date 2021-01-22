8460a9f371f6b96e3137639275322d731b70c937
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2021-01-22 20:31:47 +0000

    Voting link added

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,4 @@
+# Code from https://medium.com/@wintermeyer/authentication-from-scratch-with-rails-5-2-92d8676f6836
 class SessionsController < ApplicationController
   def new
   end
@@ -6,7 +7,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, notice: "Logged in!"
+      redirect_to '/votes/new'
     else
       flash.now[:alert] = "Email or password is invalid"
       render "new"
