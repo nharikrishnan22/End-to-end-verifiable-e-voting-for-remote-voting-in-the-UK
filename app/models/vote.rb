a44f10e13df28a6618aade6a8089c80145d71860
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2021-01-23 07:24:26 +0000

    Minor changes made

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -7,13 +7,12 @@ class Vote < ApplicationRecord
     p = 11
     q = 5
     g = 7
-    x = SecureRandom.random_number(1..q-1)
+    x = 2 # should be random for each election but this simplifies matters
     g_2 = (g ** x) % p # second generator (g2 in DRE-ip paper)
-    ri = SecureRandom.random_number(1..q-1) # generate ri in Zq
+    ri = 3 # should be random, this simplifies stuff, will need to change later
     self.big_ri = (g_2 ** ri) % p
     self.big_zi = ((g ** ri) % p) * ((g ** self.vote) % p)
     self.status = "Confirmed"
     self.pwf = (self.big_ri == ((g_2 ** ri) % p) && self.big_zi == ((g ** ri) % p)) || (big_ri == ((g_2 ** ri) % p) && self.big_zi.div(g)  == ((g ** ri) % p))
-    self.vote = 0 # vote reset after voting to preserve anonymity
   end
 end
