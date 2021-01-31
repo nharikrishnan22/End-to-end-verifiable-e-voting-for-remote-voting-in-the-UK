880c3ec3ba50b047e7ca0738863dbc0ac25ff46e
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2021-01-31 03:03:16 +0000

    Created a basic test for users and votes

--- test/models/vote_test.rb
+++ test/models/vote_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not save vote without details" do
+    vote = Vote.new
+    assert_not vote.save, "Saved a vote without any details" # error message is on this line
+  end
 end
