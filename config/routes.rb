e6ab469aa99c2eff5571164644b67f54ad88681f
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2021-03-04 05:09:50 +0000

    No route matches [GET] "/sessions" fixed

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,9 @@ Rails.application.routes.draw do
   
   resources :users
   resources :sessions, only: [:new, :create]
-
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
+  get '/signup', to: 'users#new', as: 'signup'
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/sessions', to: 'sessions#create', as: 'session'
 
 
 end
