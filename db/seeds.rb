35b502d70def464d95b8b7a7eda0b93cb99175b2
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2021-04-11 01:15:40 +0100

    Minor change

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,4 @@ user = User.create(
   {email: "jdoe@example.com", dateofbirth: "1991-04-10", firstname: "John", lastname: "Doe", password: "johndoe91"}
 )
 user.signature.attach(io: File.open('johndoe.png'),
-filename: "johndoe.png", content_type: "image/jpg")
+filename: "johndoe.png", content_type: "image/png")
