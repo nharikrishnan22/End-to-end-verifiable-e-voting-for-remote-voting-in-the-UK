b831156cc701c3427a85d2220e812d1867099824
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2021-04-11 01:15:12 +0100

    Example user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+user = User.create(
+  {email: "jdoe@example.com", dateofbirth: "1991-04-10", firstname: "John", lastname: "Doe", password: "johndoe91"}
+)
+user.signature.attach(io: File.open('johndoe.png'),
+filename: "johndoe.png", content_type: "image/jpg")
