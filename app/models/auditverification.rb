af2f680975d858af8d697ee968dc29fbf72fe64c
Neeraj Hari Krishnan <nhkrishnan4@gmail.com>
2021-04-18 22:17:50 +0100

    Make fields mandatory for audit verification and well-formedness verification

--- app/models/auditverification.rb
+++ app/models/auditverification.rb
@@ -1,2 +1,10 @@
 class Auditverification < ApplicationRecord
+    validates :big_ri, presence: true
+    validates :big_zi, presence: true
+    validates :g1_x, presence: true
+    validates :g1_y, presence: true
+    validates :g2_x, presence: true
+    validates :g2_y, presence: true
+    validates :vote, presence: true
+    validates :ri, presence: true
 end
--- app/models/wellformednessverification.rb
+++ app/models/wellformednessverification.rb
@@ -1,2 +1,14 @@
 class Wellformednessverification < ApplicationRecord
+    validates :c_1, presence: true
+    validates :c_2, presence: true
+    validates :r_1, presence: true
+    validates :r_2, presence: true
+    validates :g1_x, presence: true
+    validates :g1_y, presence: true
+    validates :g2_x, presence: true
+    validates :g2_y, presence: true
+    validates :big_ri_x, presence: true
+    validates :big_ri_y, presence: true
+    validates :big_zi_x, presence: true
+    validates :big_zi_y, presence: true
 end
